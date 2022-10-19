29053b978e257925021f00287b0e0b4138547044
Sammy Mureithi <sammymureithi20@gmail.com>
2022-10-19 10:54:49 +0300

    Bottom tab navigation setUp successfully

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import CartScreen from './Screens/CartScreen';
+import HomeScreen from './Screens/HomeScreen';
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+const BottomTab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <BottomTab.Navigator screenOptions={( { route } ) => ( {
+        tabBarIcon: ( { focused, color, size } ) => {
+          let iconName;
+          if (route.name ==="Home") {
+            iconName=focused? "home":"home-outline";
+          }
+          else {
+            iconName=focused ? "cart": "cart-outline";
+          }
+          return <Ionicons name={iconName} color={color} size={30}/>
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarStyle:{backgroundColor:"green"}
+      })}>
+        <BottomTab.Screen name='Home' component={HomeScreen} options={{ headerShown:false}} />
+        <BottomTab.Screen name='Cart' component={CartScreen} options={{ headerShown:false}} />
+      </BottomTab.Navigator>
+    </NavigationContainer>
+   
   );
 }
 
---
+++ Screens/DetailedDrinkScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function DetailedDrinkScreen() {
+  return (
+      <View>
+          <Text>Detailed Screen</Text>
+    </View>
+  )
+}
+
+export default DetailedDrinkScreen
