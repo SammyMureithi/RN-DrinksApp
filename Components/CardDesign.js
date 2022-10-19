8e514124c592fcc4fb8c058f231427172b090ccf
Sammy Mureithi <sammymureithi20@gmail.com>
2022-10-19 13:57:13 +0300

    Detailed Design successfully

--- Components/CardDesign.js
+++ Components/CardDesign.js
@@ -1,34 +1,36 @@
 import { Avatar } from '@react-native-material/core';
 import React from 'react'
-import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Card } from 'react-native-paper';
-function CardDesign({navigation}) {
+function CardDesign({navigation,handleClick}) {
     return (
-        <Card >
-            <Card.Content>
-            <View >
-            <View style={{display:"flex",flexDirection:"row",padding:10,alignItems:"center"}}>
-                <Avatar image={require( "../assets/bg2.jpg" )} />
-                <View style={{marginLeft:10}}>
-                    <Text style={{fontWeight:"bold"}}>Drink Name</Text>
-                <Text style={{fontStyle:"italic"}}>1/12/1989</Text>
-                </View>
-            </View>
-            <View>
-                <Text>
-                    You will need to add some optional modules in android/app/build.gradle,
-                    depending on the needs of your app.
-                </Text>
-            </View>
-            <View style={{ width: "100vw", height: "50vh" }}><Image source={require( "../assets/bg1.jpg" )}
-                style={{ width: "100%", height: "100%" }} /></View>
-            <View style={{display:"flex",flexDirection:"row",justifyContent:"space-between",margin:30,marginTop:10}}>
-                <Image style={{width:20,height:20}}
-                    source={require( "../assets/like.png" )} />
-                <Image source={require( "../assets/cart.png" )} style={{width:20,height:20}}/>
-            </View>
-        </View>
-            </Card.Content>
+        <Card>
+            <TouchableOpacity onPress={() => handleClick( navigation )}>
+                <Card.Content>
+                    <View >
+                        <View style={{ display: "flex", flexDirection: "row", padding: 10, alignItems: "center" }}>
+                            <Avatar image={require( "../assets/bg2.jpg" )} />
+                            <View style={{ marginLeft: 10 }}>
+                                <Text style={{ fontWeight: "bold" }}>Drink Name</Text>
+                                <Text style={{ fontStyle: "italic" }}>1/12/1989</Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Text>
+                                You will need to add some optional modules in android/app/build.gradle,
+                                depending on the needs of your app.
+                            </Text>
+                        </View>
+                        <View style={{ width: "100vw", height: "50vh" }}><Image source={require( "../assets/bg1.jpg" )}
+                            style={{ width: "100%", height: "100%" }} /></View>
+                        <View style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", margin: 30, marginTop: 10 }}>
+                            <Image style={{ width: 20, height: 20 }}
+                                source={require( "../assets/like.png" )} />
+                            <Image source={require( "../assets/cart.png" )} style={{ width: 20, height: 20 }} />
+                        </View>
+                    </View>
+                </Card.Content>
+            </TouchableOpacity>
         </Card>
    
   )
--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { Button, Image, StyleSheet, Text, View,ScrollView } from 'react-native';
+import { Button, Image, StyleSheet, Text, View,ScrollView, TouchableOpacity } from 'react-native';
+import { Card } from 'react-native-paper';
 import CardDesign from '../Components/CardDesign';
 function HomeScreen( { navigation } ) {
+    function handleNavigate(navigation) {
+        navigation.navigate( "Detailed" )
+    }
   return (
-      <ScrollView>
-          <CardDesign />
-          <CardDesign/>
-          <CardDesign />
+      <ScrollView >
+          <CardDesign  handleClick={()=>handleNavigate( navigation )}/>
+          <CardDesign  handleClick={()=>handleNavigate( navigation )}/>
+          <CardDesign  handleClick={()=>handleNavigate( navigation )}/>
    </ScrollView>
   )
 }
